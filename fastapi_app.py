f996af60688870221d14dac55b5729bd7050e19f
AI Anytime <sonu1000raw@gmail.com>
2023-11-27 23:39:41 +0530

    Add files via upload

---
+++ fastapi_app.py
@@ -0,0 +1,84 @@
+from langchain import PromptTemplate, LLMChain
+from langchain.llms import CTransformers
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.document_loaders import PyPDFLoader
+from fastapi import FastAPI, Request, Form, Response
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.encoders import jsonable_encoder
+
+import os
+import json
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+local_llm = "neural-chat-7b-v3-1.Q4_K_M.gguf"
+
+config = {
+'max_new_tokens': 1024,
+'repetition_penalty': 1.1,
+'temperature': 0.1,
+'top_k': 50,
+'top_p': 0.9,
+'stream': True,
+'threads': int(os.cpu_count() / 2)
+}
+
+llm = CTransformers(
+    model=local_llm,
+    model_type="mistral",
+    lib="avx2", #for CPU use
+    **config
+)
+
+print("LLM Initialized....")
+
+prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+model_name = "BAAI/bge-large-en"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+
+
+prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
+
+load_vector_store = Chroma(persist_directory="stores/pet_cosine", embedding_function=embeddings)
+
+retriever = load_vector_store.as_retriever(search_kwargs={"k":1})
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/get_response")
+async def get_response(query: str = Form(...)):
+    chain_type_kwargs = {"prompt": prompt}
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True, chain_type_kwargs=chain_type_kwargs, verbose=True)
+    response = qa(query)
+    print(response)
+    answer = response['result']
+    source_document = response['source_documents'][0].page_content
+    doc = response['source_documents'][0].metadata['source']
+    response_data = jsonable_encoder(json.dumps({"answer": answer, "source_document": source_document, "doc": doc}))
+    
+    res = Response(response_data)
+    return res
